8406873e14c43ff1f9649341f3733a190d97c8f8
marie428 <marie@tdtran.cz>
2026-06-11 20:18:59 +0200

    changing the color palette, adding legend symbology

--- src/health_access/clustering/visualization.py
+++ src/health_access/clustering/visualization.py
@@ -1,11 +1,13 @@
 import geopandas as gpd
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-import os
 from pathlib import Path
 
-def create_cluster_map(gdf, config, project_root):
+
+def create_cluster_map(gdf, config, project_root, show_legend=False):
     """
     Renders the clustered GeoDataFrame and saves it as a PNG.
+    ### ADD MORE DESRCIPTION
     """
     # Resolve the output path
     processed_dir = project_root / config['paths']['processed_dir']
@@ -15,16 +17,40 @@ def create_cluster_map(gdf, config, project_root):
     # Create the plot
     fig, ax = plt.subplots(figsize=(12, 10))
     
+    # Define a color palette for clusters
+    clusters = sorted(gdf['cluster'].unique())
+    colors = [
+            "#e6194b",  # red
+            "#4363d8",  # blue
+            "#f58231",  # orange
+            "#3cb44b",  # green
+            "#ffe119",  # yellow
+            "#f032e6",  # pink
+            "#911eb4",  # purple
+            "#42d4f4",  # cyan
+            "#a9a9a9",  # grey
+        ][:len(clusters)]
+
+
+    gdf['color'] = gdf['cluster'].apply(lambda x: colors[clusters.index(x)])
     gdf.plot(
-        column='cluster', 
-        cmap=config['visualization']['cmap'], 
+        color=gdf['color'],
         legend=False, 
         ax=ax, 
         edgecolor='white', 
         linewidth=0.5
     )
+
+    # Add legend if requested buy the user via CLI
+    if show_legend:
+        patches = [
+            mpatches.Patch(color=colors[i], label=f"Cluster {i + 1}")
+            for i, cluster in enumerate(clusters)
+        ]
+        ax.legend(handles=patches, loc="upper right", frameon=True)
     
-    plt.title(config['visualization']['title'], fontsize=15)
+    n = config['clustering']['n_clusters']
+    plt.title(f"{config['visualization']['title']} --- {n} clusters", fontsize=15)
     ax.set_axis_off() 
     
     # Save the file
